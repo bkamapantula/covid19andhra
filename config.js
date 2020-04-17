8d25081bfc0237607eb4b58c15f2355dc5c943d2
Bhanu K <bhanu.kamapantula@gramener.com>
2020-04-17 11:14:31 +0530

    ENH: changes for covid19andhra

--- config.js
+++ config.js
@@ -2,18 +2,11 @@ var config = {
   "en": {
     "toppanel": {
       "links": [
-        {"text": "By BehanBox", "url": "https://www.behanbox.com/"},
-        {"text": "Tweet", "url": "https://twitter.com/intent/tweet?url=http%3A%2F%2Fcovid19-india-response.surge.sh%2F&via=behanbox&text=Indian%20response%20to%20Covid-19%20spread&hashtags=covid19india%2C%20covid19"},
-        {"text": "Data", "url": "https://docs.google.com/spreadsheets/d/1_JIaWdsm6fYPStN2fin0wt4ad2JSn8Sqt5bNeBZ4riM/edit?usp=sharing"}
+        {"text": "By Suresh Ediga", "url": "https://twitter.com/suresh_ediga"},
+        {"text": "Data", "url": "https://docs.google.com/spreadsheets/d/1kQZkch8S36W4JmThVGX3TvV0RpupCBSVlPxkrlpyIxg/edit#gid=598965942"}
       ],
-      "title": "Covid-19 India Response"
+      "title": "Covid-19 Andhra Pradesh Relief Camps"
     },
-    "cover": "How is India responding to the COVID-19 pandemic? What steps are Union and State governments taking to protect their citizens. We track all the policy measures- economic, business, welfare- here.",
-    "filters": [
-      {"text": "Government Orders", "url": "?view=Government Order"},
-      {"text": "Welfare Measures", "url": "?view=Welfare Measures"},
-      {"text": "Data/Technology/Surveillance", "url": "?view=Surveillance"}, 
-      {"text": "Lockdown Deaths", "url": "?view=Lockdown Deaths"}
-    ]
+    "cover": "District-wise relief camps with contact information"
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var misc_categories_uniq
-var url = "https://docs.google.com/spreadsheets/d/1_JIaWdsm6fYPStN2fin0wt4ad2JSn8Sqt5bNeBZ4riM/edit?usp=sharing"
+var url = "https://docs.google.com/spreadsheets/d/1kQZkch8S36W4JmThVGX3TvV0RpupCBSVlPxkrlpyIxg/edit?usp=sharing"
 var g1_url = g1.url.parse(location.href)
 var sheets_global
 
@@ -34,39 +33,18 @@ function render_page(arrange_by) {
     by_date = _.groupBy(filtered_elements_dates, "when")
     data = {data: by_date, _by: 'date'}
   } else {  // state
-    by_location = _.groupBy(sheets_global, "location")
+    by_location = _.groupBy(sheets_global, "DISTRICT NAME")
     g_sorted = _(by_location).toPairs().sortBy(0).fromPairs().value()
-    data = {data: g_sorted, _by: 'state'}
+    data = {data: g_sorted, _by: 'DISTRICT NAME'}
   }
   var tmpl = _.template($("#item-template").html())
+  // console.log("template data", data)
   $('#updates').html(tmpl(data))
-  render_subset() // if applicable based on URL param
-
-  misc_categories = _.uniq(_.map(sheets_global, function(item) { return item.misc }))
-  m = _.map(misc_categories, function(item) { return item.split(',') })
-  misc_categories_uniq = _.uniq([].concat.apply([], m))
-  // trim " PDS" to "PDS" (and others), take a unique set of them
-  misc_categories_uniq = _.uniq(_.map(misc_categories_uniq, function(item) { return item.trim() }))
-  $('#autocomplete').autocomplete({
-    source: misc_categories_uniq,
-    minLength: 0,
-    select: function(evt, ui) {
-      // search by category
-      toggle_state_cards(ui.item.value, 'cat')
-      g1_url.update({cat: ui.item.value, view: null}, 'cat=toggle&view=del')
-      history.pushState({}, '', g1_url.toString())
-      $("[data-label]").removeClass('border-selected')
-    }
-  }).bind('focus', function () {
-    // binds focus with autocomplete
-    $(this).autocomplete("search")
-  })
 }
 
 function render_static_content() {
   var tmpl = _.template($("#static-content").html())
-  // config = {config: config}
-  console.log(config)
+  // console.log(config)
   $("#content").html(tmpl({config: config.en}))
 }
 
@@ -77,16 +55,12 @@ function init(arrange_by) {
   Tabletop.init({
     key: url,
     callback: function(sheets_data, tabletop) {
-      // data in events sheet
-      translations = sheets_data.translations.elements
-      sheets_global = _.filter(sheets_data.events.elements, function(each_row) {
+      // data in ANANTAPUR sheet
+      sheets_global = _.filter(sheets_data.ANANTAPUR.elements, function(each_row) {
         return each_row['status'] != "0";
       })
       render_static_content()
       render_page()
-      $('.dropdown-trigger').dropdown()
-      g1_url.update({lang: 'en'}, 'toggle')
-      history.pushState('', {}, g1_url.toString())
     },
     simpleSheet: false
   })
@@ -123,22 +97,4 @@ $('body').on('click', 'a.btn', function() {
   toggle_state_cards(label, 'view')
   g1_url.update({view: label, cat: null}, 'view=toggle&cat=del')
   history.pushState({}, '', g1_url.toString())
-}).on('click', '[data-arrange]', function() {
-  _by = $(this).data('arrange')
-  render_page(_by)
-}).on('click', '.lang', function() {
-  lang = $(this).data("lang")
-  g1_url.update({lang: lang}, 'toggle')
 })
-
-// if url has attributes (persistent share) render accordingly
-function render_subset() {
-  if(Object.keys(g1_url.searchKey).length > 0) {
-    if(Object.keys(g1_url.searchKey).indexOf('cat') > -1) {  // ?cat=
-      toggle_state_cards(g1_url.searchKey['cat'], 'cat')
-    } if(Object.keys(g1_url.searchKey).indexOf('view') > -1) {  // ?view=
-      toggle_state_cards(g1_url.searchKey['view'], 'view')
-      $("[data-label='" + g1_url.searchKey.view + "']").addClass('border-selected')
-    }
-  }
-}
